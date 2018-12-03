699cd6c6582238e6de3cc943501f77314a3697eb
Sahra Vadrot <apexp@hotmail.fr>
2018-12-03 15:23:54 +0100

    constant var, search map file and main check

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # sudoku
 
-run 
+run on Linux
 - `gcc -o main main.c` 
 - `./main`
+
+run on Windows(Cygwin64)
+- `g++ -std=gnu++11 main.c`
+- `./a.exe sudoku.txt`
 
--- main.c
+++ main.c
@@ -2,25 +2,59 @@
 #include <stdlib.h>
 #include <limits.h>
 
-
 #define rows 9
 #define cols 9
+#define values 9
+#define box_width 3
 
 int grid[rows][cols];
 
-int loadSudoku(){
+int sudokuFile (const char *path) {
+	FILE *file = fopen(path, "r");
+	
+	if (!file)
+		return 0;
+	
+	for (int row = 0; row < rows; row++) {
+		for (int col = 0; col < cols; col++) {
+			fscanf(file, "%d", &grid[row][col]);
+		}
+	}
+	
+	fclose(file);
+	
+	return 1;
+}
+
+int valid (int row, int col, int val) {
 
 }
 
-int solve(){
-    
-}
-
-int valid(){
+int solve (int row, int col) {
 
 }
 
-int main(){
-    printf("hello\n");
-    return EXIT_SUCCESS;
+int main (int argc, const char * argv[]) {
+	if (argc != 2) {
+		printf("Usage: sudoku /path/to/sudoku.txt\n");
+		
+		return EXIT_SUCCESS;
+	}
+	
+	char realPath[PATH_MAX + 1];
+	char *res = realpath(argv[1], realPath);
+	
+	if (!res) {
+		printf("Invalid sudoku path!\n");
+		
+		return EXIT_FAILURE;
+	}
+	
+	if (!sudokuFile(realPath)) {
+		printf("Couldn't load the sudoku!\n");
+		
+		return EXIT_FAILURE;
+	}
+	
+	return EXIT_SUCCESS;
 }
